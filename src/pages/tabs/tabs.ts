d6c7caba8ff936612ebb1a27dd76977c137eca22
Chet Hill <chetdhill@gmail.com>
2018-07-04 16:19:33 -0400

    Create Workouts and Add Workout Pages

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 
+import { AddWorkoutPage } from '../add-workout/add-workout';
+import { WorkoutsPage } from '../workouts/workouts';
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = WorkoutsPage;
+  tab2Root = AddWorkoutPage;
+  tab3Root = AboutPage;
 
   constructor() {
 
